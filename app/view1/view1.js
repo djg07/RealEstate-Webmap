17f40a6cceaf536f54e941ca04502d94225b6110
djg07 <daniel.galati@ryerson.ca>
2014-12-14 20:44:23 -0500

    Added basemap

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -17,7 +17,20 @@ angular.module('myApp.view1', ['ngRoute'])
 })
 
 .controller('MapController', function($scope) {  //working
-  $scope.mapTitle = 'RE App';
+  //$scope.mapTitle = 'RE App';
+      var map = new ol.Map({
+        target: 'map',
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.MapQuest({layer: 'sat'})
+          })
+        ],
+        view: new ol.View({
+          center: ol.proj.transform([-79.45,43.65], 'EPSG:4326', 'EPSG:3857'),
+          zoom: 11
+        })
+      });
+
   // ...
 });
 
