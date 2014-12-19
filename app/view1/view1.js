3e54c162854aa0d053802af9c89dc90dcc1f8f00
djg07 <d.galati54@gmail.com>
2014-12-19 18:42:25 -0500

    Small fixes

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -18,7 +18,7 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .controller('MapController', function($scope) {  //working //Separate the controllers out into their own files!
   //$scope.mapTitle = 'RE App';                                 //see tut at 13:11
-<<<<<<< HEAD
+
 
 
       L.mapbox.accessToken = 'pk.eyJ1IjoiZGdhbGF0aSIsImEiOiI5MVd5dV84In0.HBORjJfOqTCyqoMlKukIvA';
@@ -26,26 +26,9 @@ angular.module('myApp.view1', ['ngRoute'])
           .setView([43.7, -79.45], 11);
 
 
-=======
-      var map = new ol.Map({
-        target: 'map',
-        layers: [
-          new ol.layer.Tile({
-            title: "Global Imagery",
-            source: new ol.source.TileWMS({
-              url: 'http://maps.opengeo.org/geowebcache/service/wms',
-              params: {LAYERS: 'openstreetmap', VERSION: '1.1.1'}
-            })
-          })
-        ],
-        view: new ol.View({
-          projection: 'EPSG:4326',
-          center: [0, 0],
-          zoom: 0,
-          maxResolution: 0.703125
-        })
-      });
->>>>>>> 979094abad846ac3e7843c16995de8c49aa61250
+
+      
+
   // ...
 })
 
