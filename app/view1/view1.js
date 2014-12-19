596e65966c87f27f3289309d11bca790febdc215
djg07 <d.galati54@gmail.com>
2014-12-19 15:54:08 -0500

    changed to mapbox

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -18,18 +18,12 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .controller('MapController', function($scope) {  //working //Separate the controllers out into their own files!
   //$scope.mapTitle = 'RE App';                                 //see tut at 13:11
-      var map = new ol.Map({
-        target: 'map',
-        layers: [
-          new ol.layer.Tile({
-            source: new ol.source.MapQuest({layer: 'sat'})
-          })
-        ],
-        view: new ol.View({
-          center: ol.proj.transform([-79.45,43.65], 'EPSG:4326', 'EPSG:3857'),
-          zoom: 11
-        })
-      });
+
+
+      L.mapbox.accessToken = 'pk.eyJ1IjoiZGdhbGF0aSIsImEiOiI5MVd5dV84In0.HBORjJfOqTCyqoMlKukIvA';
+      var map = L.mapbox.map('map', 'dgalati.kgnfc438')
+          .setView([43.7, -79.45], 11);
+
 
   // ...
 });
